4095f96f6355cf24e19d2fd3d7c157845b8b5354
ahmadammmoura <ammoura575@gmail.com>
2021-03-25 17:02:54 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+el = document.getElementById('line')
+
+for(let i = 0 ; i < 20 ; i++){
+    img = document.createElement('img').setAttribute('src','img/hr-img.png');
+    el.appendChild(img);
+}
+
+
+
+
